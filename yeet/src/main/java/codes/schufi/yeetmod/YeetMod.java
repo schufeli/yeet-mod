02a14864b86f9b4280daa73b86a3caf19158bba3
schufeli <me@schufi.codes>
2022-10-25 09:21:44 +0200

    Added initial project files

---
+++ yeet/src/main/java/codes/schufi/yeetmod/YeetMod.java
@@ -0,0 +1,12 @@
+package codes.schufi.yeetmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(YeetMod.MODID)
+public class YeetMod {
+
+    // Global Settings
+    public static final String MODID = "yeetmod";
+
+    public YeetMod() { }
+}
