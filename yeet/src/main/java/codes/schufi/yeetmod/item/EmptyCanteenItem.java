29dfe1fdb206a05a77cd9b599f1ff948d87c0645
schufeli <me@schufi.codes>
2022-11-15 10:07:25 +0100

    Added basic empty_canteen item

---
+++ yeet/src/main/java/codes/schufi/yeetmod/item/EmptyCanteenItem.java
@@ -0,0 +1,9 @@
+package codes.schufi.yeetmod.item;
+
+import net.minecraft.world.item.Item;
+
+public class EmptyCanteenItem extends Item {
+    public EmptyCanteenItem(Item.Properties properties) {
+        super(properties);
+    }
+}
