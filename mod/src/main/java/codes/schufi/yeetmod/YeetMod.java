f1b6200de2da8bede7c9e537b45947540b544b12
schufeli <me@schufi.codes>
2022-12-15 21:32:31 +0100

    Added initial mdk forge files

---
+++ mod/src/main/java/codes/schufi/yeetmod/YeetMod.java
@@ -0,0 +1,34 @@
+package codes.schufi.yeetmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(YeetMod.MOD_ID)
+public class YeetMod {
+    public static final String MOD_ID = "yeetmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public YeetMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        bus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) { }
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) { }
+    }
+}
