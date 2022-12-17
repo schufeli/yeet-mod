d968fdd11a656c9e755bfb23b4af1acc502206f9
schufeli <me@schufi.codes>
2022-12-17 12:24:00 +0100

    Added GlassBottleItem

---
+++ mod/src/main/java/codes/schufi/yeetmod/item/GlassBottleItem.java
@@ -0,0 +1,9 @@
+package codes.schufi.yeetmod.item;
+
+import net.minecraft.world.item.Item;
+
+public class GlassBottleItem extends Item {
+    public GlassBottleItem(Item.Properties properties) {
+        super(properties);
+    }
+}
