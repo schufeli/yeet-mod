d791c48c3ebdcd91b97cbaeecc3ab688401117ca
schufeli <me@schufi.codes>
2022-12-17 12:23:16 +0100

    Added WaterBottleItem

---
+++ mod/src/main/java/codes/schufi/yeetmod/item/WaterBottleItem.java
@@ -0,0 +1,9 @@
+package codes.schufi.yeetmod.item;
+
+import net.minecraft.world.item.Item;
+
+public class WaterBottleItem extends Item {
+    public WaterBottleItem(Item.Properties properties) {
+        super(properties);
+    }
+}
