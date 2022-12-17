53220661a3f03ad4efd70ead6dbd6222915f2119
schufeli <me@schufi.codes>
2022-12-17 12:23:34 +0100

    Added DirtyBottleItem

---
+++ mod/src/main/java/codes/schufi/yeetmod/item/DirtyBottleItem.java
@@ -0,0 +1,9 @@
+package codes.schufi.yeetmod.item;
+
+import net.minecraft.world.item.Item;
+
+public class DirtyBottleItem extends Item {
+    public DirtyBottleItem(Item.Properties properties) {
+        super(properties);
+    }
+}
